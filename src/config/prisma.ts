0d78987bc9de22c48683b774cc0a534f6ea93bfc
Rosine012 <rosinetumukunde088@gmail.com>
2026-05-05 15:22:41 +0200

    new

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,6 +1,6 @@
 import pg from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated";
 
 const pool = new pg.Pool({
   connectionString: process.env["DATABASE_URL"],
